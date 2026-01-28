86168e6e38d63955dec9977f68f5d7024d8ffde1
RainkQ <rainkq@gmail.com>
2026-01-28 20:05:08 +0900

    feat: release v1.8.2 with p2p fix and improved release script

---
+++ scripts/release.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process');
+const { version } = require('../package.json');
+
+const tagName = `v${version}`;
+const command = `gh release create ${tagName} dist/*.js --repo RainkQ/Make-Bilibili-Great-Than-Ever-Before --generate-notes`;
+
+try {
+  console.log(`Executing: ${command}`);
+  execSync(command, { stdio: 'inherit' });
+  console.log(`Successfully created GitHub release for tag ${tagName}`);
+} catch (error) {
+  console.error(`Failed to create GitHub release for tag ${tagName}.`);
+  // The error from execSync will be printed to stderr because of 'inherit'
+  process.exit(1);
+}
